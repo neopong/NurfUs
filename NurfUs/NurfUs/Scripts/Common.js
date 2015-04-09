f5db61a5d6e466bb166877710ea4be0a19d66d10
neopong <neopong@gmail.com>
2015-04-09 10:03:49 -0600

    Layout, js and css cleanup

---
+++ NurfUs/NurfUs/Scripts/Common.js
@@ -0,0 +1,69 @@
+﻿$(document).ready(function () {
+    if (!String.prototype.supplant) {
+        String.prototype.supplant = function (o) {
+            return this.replace(/{([^{}]*)}/g,
+                function (a, b) {
+                    var r = o[b];
+                    return typeof r === 'string' || typeof r === 'number' ? htmlEncode(r) : htmlEncode(a);
+                }
+            );
+        };
+    }
+
+});
+
+function htmlEncode(message) {
+    return $('<div />').text(message).html();
+}
+
+function describeTime(milliseconds) {
+    milliseconds = milliseconds / 1;
+
+    var totalSeconds = Math.floor(milliseconds / 1000);
+    var seconds = totalSeconds % 60;
+    var totalMinutes = Math.floor(totalSeconds / 60);
+    var minutes = totalMinutes % 60;
+    var hours = Math.floor(totalMinutes / 60);
+
+    var timeDisplay = "";
+
+    if (hours <= 10) {
+        var pad = "00";
+
+        var timePart;
+
+        if (hours > 0) {
+            timePart = "" + hours;
+            timeDisplay = pad.substring(0, pad.length - timePart.length) + timePart + ":";
+        }
+
+        timePart = "" + minutes;
+        timeDisplay += pad.substring(0, pad.length - timePart.length) + timePart + ":";
+
+        timePart = "" + seconds;
+        timeDisplay += pad.substring(0, pad.length - timePart.length) + timePart;
+    } else {
+        timeDisplay = "--**Waiting for new match**--";
+    }
+
+    return timeDisplay;
+}
+
+var pageTimer = setInterval(function () { myTimer() }, 1000);
+
+function myTimer() {
+    $(".timer").each(function (index, element) {
+        var milliseconds = element.getAttribute("data-x-started") / 1;
+
+        $(element).text(describeTime(milliseconds));
+
+        element.setAttribute("data-x-started", milliseconds + 1000);
+    });
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
---
+++ NurfUs/NurfUs/Scripts/Page/Main.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    var hub = $.connection.nurfUsHub;
+
+    var timeLeftTemplate = '<div class="badge timer" data-x-started="{SecondsElapsed}"></div>';
+
+    hub.client.newMatch = function (gameDisplay) {
+        $("#gameDetail").empty().append(timeLeftTemplate.supplant(gameDisplay));
+        $("#participant1").empty().append('<img src="/Images/Champion/' + gameDisplay.BlueTeam[0].ChampionImage + '"\>');
+        $("#participant2").empty().append('<img src="/Images/Champion/' + gameDisplay.BlueTeam[1].ChampionImage + '"\>');
+        $("#participant3").empty().append('<img src="/Images/Champion/' + gameDisplay.BlueTeam[2].ChampionImage + '"\>');
+        $("#participant4").empty().append('<img src="/Images/Champion/' + gameDisplay.BlueTeam[3].ChampionImage + '"\>');
+        $("#participant5").empty().append('<img src="/Images/Champion/' + gameDisplay.BlueTeam[4].ChampionImage + '"\>');
+        $("#participant6").empty().append('<img src="/Images/Champion/' + gameDisplay.PurpleTeam[0].ChampionImage + '"\>');
+        $("#participant7").empty().append('<img src="/Images/Champion/' + gameDisplay.PurpleTeam[1].ChampionImage + '"\>');
+        $("#participant8").empty().append('<img src="/Images/Champion/' + gameDisplay.PurpleTeam[2].ChampionImage + '"\>');
+        $("#participant9").empty().append('<img src="/Images/Champion/' + gameDisplay.PurpleTeam[3].ChampionImage + '"\>');
+        $("#participant10").empty().append('<img src="/Images/Champion/' + gameDisplay.PurpleTeam[4].ChampionImage + '"\>');
+    };
+
+    hub.client.broadcastMessage = function (name, message) {
+        // Html encode display name and message. 
+        var encodedName = htmlEncode(name);
+        var encodedMsg = htmlEncode(message);
+        // Add the message to the page. 
+        $('#discussion').prepend('<li><strong>' + encodedName
+            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+    };
+    // Get the user name and store it to prepend to messages.
+    $('#displayname').val("Guest( " + prompt('Enter your name:', '') + " )");
+    // Set initial focus to message input box.  
+    $('#message').focus();
+
+    $(window).keydown(function (event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            $('#sendmessage').click();
+            return false;
+        }
+    });
+
+    $.connection.hub.start().done(function () {
+        hub.server.getCurrentMatch();
+
+        $('#sendmessage').click(function () {
+            // Call the Send method on the hub. 
+            hub.server.send($('#displayname').val(), $('#message').val());
+            // Clear text box and reset focus for next comment. 
+            $('#message').val('').focus();
+        });
+    });
+});
