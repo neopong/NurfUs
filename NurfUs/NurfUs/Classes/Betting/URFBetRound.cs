05b7d496a9d51a6fba2486fb965241b26298c811
tyler-thomas <tyler.thomas.127@gmail.com>
2015-04-10 19:59:07 -0700

    Beginning Bet architecture

---
+++ NurfUs/NurfUs/Classes/Betting/URFBetRound.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NurfUs.Classes.Betting
+{
+
+    public class UserBet
+    {
+        int BetOptionIndex
+        {
+            get;
+            set;
+        }
+
+        int BetAmount
+        {
+            get;
+            set;
+        }
+    }
+    public class URFBetRound : IBetRound
+    {
+        List<BetOption> BetOptions
+        {
+            get;
+            set;
+        }
+
+        //UserId, with bit option Index
+        Dictionary<String, int> UserBets
+        {
+            get;
+            set;
+        }
+
+        public int WinningBetIndex
+        {
+            get;
+            set;
+        }
+
+        public URFBetRound(List<BetOption> betOptions, int winningBetIndex)
+        {
+            BetOptions = betOptions;
+            WinningBetIndex = winningBetIndex;
+        }
+
+        public void AddUserBet(String userId, int betOptionIndex)
+        {
+            if (UserBets.ContainsKey(userId))
+            {
+                UserBets[userId] = betOptionIndex;
+            }
+            else
+            {
+                UserBets.Add(userId, betOptionIndex);
+            }
+        }
+
+    }
+}
