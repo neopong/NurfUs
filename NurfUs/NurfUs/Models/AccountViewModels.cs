2e18021c7e3e4c66de49060ec0cab5d3abb613b6
tyler-thomas <tyler.thomas.127@gmail.com>
2015-04-16 19:07:35 -0700

    added guest validation on register

--- NurfUs/NurfUs/Models/AccountViewModels.cs
+++ NurfUs/NurfUs/Models/AccountViewModels.cs
@@ -73,6 +73,7 @@ namespace NurfUs.Models
         [Display(Name="Display Name")]
         [MaxLength(256, ErrorMessage="Your display name must be less than 256 characters long")]
         [MinLength(2, ErrorMessage = "Your display name must be at least 2 characters long")]
+        [RegularExpression(@"^((?!Guest).)*$", ErrorMessage = "Invalid Display Name")]
         public string UserName { get; set; }
 
         [Required]
