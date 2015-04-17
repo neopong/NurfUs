1d468e1b83029783235fc4b1642b0573703880a6
unknown <ScottK@Station25.1099pro.com>
2015-04-16 18:41:50 -0700

    Added NurfUsMatchPuller to repo

---
+++ NurfUsMatchPuller/NurfUsMatchPuller/Classes/API/Participant.cs
@@ -0,0 +1,24 @@
+﻿using NurfUsMatchPuller.Classes.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NurfUs.Models.API
+{
+    public class Participant
+    {
+        public bool Bot { get; set; }
+        public long ChampionId { get; set; }
+        public Mastery[] Masteries { get; set; }
+        public int ParticipantId { get; set; }
+        public long ProfileIconId { get; set; }
+        public Rune[] Runes { get; set; }
+        public long Spell1Id { get; set; }
+        public long Spell2Id { get; set; }
+        public ParticipantStats Stats { get; set; }
+        public long SummonerId { get; set; }
+        public string SummonerName { get; set; }
+        public long TeamId { get; set; }
+    }
+}
---
+++ NurfUsMatchPuller/NurfUsMatchPuller/Exceptions/ScraperFunctionInitializationException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NurfUsMatchPuller.Exceptions
+{
+    public class ScraperFunctionInitializationException : Exception
+    {
+        public ScraperFunctionInitializationException(String caller)
+            : base(String.Format("{0} failed to initialize scraper function dictionary", caller))
+        {
+
+        }
+    }
+}
